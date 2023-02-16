da4d6c33b522d1f4c24acd1ad7abb670557377dd
VitroidFPV <vitroid05@gmail.com>
2023-02-16 21:48:52 +0100

    Error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<div class="w-full flex flex-col justify-center items-center p-16 pt-64 h-fit">
+	<h1 class="text-6xl font-minecraft text-violet-500">Something went wrong!</h1>
+
+	<div class="text-2xl font-minecraft">Maybe your input was wrong, try again</div>
+</div>
